4056213a18cd59a2ac41c2312797ea9cc873cd5f
pratibhatrikole2021 <pratibhatrikole1998@gmail.com>
2021-12-25 04:31:57 -0800

    commit succesfull

--- basicprogram/AsciiValueAtoZ.java
+++ basicprogram/AsciiValueAtoZ.java
@@ -4,7 +4,7 @@ public class AsciiValueAtoZ {
 	
 	public static void main(String[] args) {
 		
-		for(int i=65; i<=90;i++) {
+		for(int i=65; i<=80;i++) {
 			System.out.println("The Ascii Value of"+(char)i+ " = "+i);
 		}
 		
