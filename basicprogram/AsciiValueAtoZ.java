8aa58368da2922556f7acafc1ddc29770179fa9c
pratibhatrikole2021 <pratibhatrikole1998@gmail.com>
2021-12-25 05:37:04 -0800

    changes succefully

--- basicprogram/AsciiValueAtoZ.java
+++ basicprogram/AsciiValueAtoZ.java
@@ -5,6 +5,7 @@ public class AsciiValueAtoZ {
 	public static void main(String[] args) {
 		
 		for(int i=65; i<=80;i++) {
+			System.out.println("convert char to ascii value");
 			System.out.println("The Ascii Value of"+(char)i+ " = "+i);
 		}
 		
